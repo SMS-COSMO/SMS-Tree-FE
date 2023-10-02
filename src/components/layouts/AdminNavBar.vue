0c628812e0527a9b05f8b1810a70cd28aaca4f79
zhangtianli2006 <zhangtianli2006@163.com>
2023-10-02 10:18:50 +0800

    [admin] add register page

---
+++ src/components/layouts/AdminNavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-menu class="left-menu" background-color="#f3f5f7" active-text-color="#4d4d4d" :router="true">
+    <el-menu-item index="/admin/register">
+      <el-icon>
+        <CirclePlus />
+      </el-icon>
+      <span>创建账户</span>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <el-icon>
+        <document />
+      </el-icon>
+      <span>Navigator Three</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon>
+        <setting />
+      </el-icon>
+      <span>Navigator Four</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style lang="scss" scoped>
+@import "~/styles/mixins.scss";
+
+.left-menu {
+  @include border-normal();
+  user-select: none;
+  border-radius: 10px;
+  overflow: none;
+  width: 250px;
+}
+
+.el-menu-item .is-active {
+  background-color: #fff !important;
+}
+</style>
