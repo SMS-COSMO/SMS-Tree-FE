44fe8400c41e00607205e6a41d626e5b2822fed6
zhangtianli2006 <zhangtianli2006@163.com>
2023-10-25 13:02:00 +0800

    feat(admin): make studentList names clickable

--- src/pages/admin/StudentListPage.vue
+++ src/pages/admin/StudentListPage.vue
@@ -2,14 +2,23 @@
   <el-card>
     <el-table :data="listData" v-loading="loading">
       <el-table-column type="selection" width="55" />
-      <el-table-column :width="150" show-overflow-tooltip prop="id" label="学号" />
+      <el-table-column :width="150" show-overflow-tooltip prop="id" label="学号">
+        <template #default="scope">
+          <span @click="visitProfile(scope.row.id)" style="cursor: pointer !important;">
+            {{ scope.row.id }}
+          </span>
+        </template>
+      </el-table-column>
       <el-table-column :width="100" show-overflow-tooltip prop="username" label="姓名" />
       <el-table-column :width="100" label="班级" />
       <el-table-column :width="400" show-overflow-tooltip label="课题" />
-      <el-table-column label="操作">
+      <el-table-column label="操作" align="right">
+        <template #header>
+          <el-input v-model="searchContent" placeholder="搜索学生" />
+        </template>
         <template #default="">
-          <el-button size="small">Edit</el-button>
-          <el-button size="small" type="danger">Delete</el-button>
+          <el-button size="small">修改</el-button>
+          <el-button size="small" type="danger">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -20,9 +29,18 @@
 import { onMounted, ref } from 'vue';
 import { RouterOutput, isTRPCClientError, trpc } from '../../api/trpc';
 import { ElMessage } from 'element-plus';
+import { useRouter } from 'vue-router';
 
 const listData = ref<RouterOutput['user']['studentList']>([]);
 const loading = ref(true);
+
+const searchContent = ref('');
+
+const router = useRouter();
+
+const visitProfile = (id: string) => {
+  router.push(`/user/${id}`);
+};
 
 onMounted(async () => {
   try {
