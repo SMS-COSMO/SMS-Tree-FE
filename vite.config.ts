db1c8c0114a8cd98b23472fa2d5508582ac206da
zhangtianli2006 <zhangtianli2006@163.com>
2023-11-16 13:06:11 +0800

    ci: add rewrite for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
-import UnoCSS from 'unocss/vite'
+import UnoCSS from 'unocss/vite';
 
-const pathSrc = path.resolve(__dirname, 'src')
+const pathSrc = path.resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,4 +27,5 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
-})
+});
+
