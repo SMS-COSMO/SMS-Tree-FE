3ab598fb9beb4650d018cad90b8e98dd8ede5bbd
zhangtianli2006 <zhangtianli2006@163.com>
2023-11-16 16:10:33 +0800

    feat(user): auto jump to login page when not logged in

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <NavBar />
+  <div id="view" class="mx-auto px-5">
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { trpc } from './api/trpc';
+import { onMounted } from 'vue';
+import { UserStore } from './stores/user';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const userStore = UserStore();
+
+onMounted(async () => {
+  try {
+    await trpc.user.tokenValidity.query({ token: userStore.accessToken });
+  } catch (err) {
+    router.push('/login');
+  }
+});
+</script>
+
+<style lang="scss">
+#view {
+  max-width: 1300px;
+  margin-top: 95px;
+}
+</style>
