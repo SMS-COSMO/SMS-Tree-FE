68988277bdf7d3ba81afef51a8a18f54fd465528
zhangtianli2006 <zhangtianli2006@163.com>
2023-11-18 09:46:10 +0800

    feat(user): add paper list in user profile

--- src/components/account/UserProfile.vue
+++ src/components/account/UserProfile.vue
@@ -21,16 +21,26 @@
       </el-card>
     </el-col>
   </el-row>
-  <el-card class="mt-5">
+  <FoldableCard class="mt-5">
     <template #header>
-      参与的项目
+      参与的论文
     </template>
-  </el-card>
+    <el-skeleton :rows="10" :loading="paperLoading" animated>
+      <div class="desktop:columns-2">
+        <div v-for="(paper, index) in papers" :key="index">
+          <paperCard :paper="paper" showAbstract />
+        </div>
+      </div>
+      <div v-if="!papers.length">
+        <el-empty :image-size="130" description="暂无论文" />
+      </div>
+    </el-skeleton>
+  </FoldableCard>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
-import { isTRPCClientError, trpc } from '../../api/trpc';
+import { PaperListOutput, isTRPCClientError, trpc } from '../../api/trpc';
 import type { UserProfileOutput } from '../../api/trpc';
 import { ElMessage } from 'element-plus';
 
@@ -39,12 +49,21 @@ const props = defineProps<{
 }>();
 
 const info = ref<UserProfileOutput>();
+const papers = ref<PaperListOutput>([]);
 const contentLoading = ref(true);
+const paperLoading = ref(true);
 
 onMounted(async () => {
   try {
     info.value = await trpc.user.profile.query({ id: props.userId });
     contentLoading.value = false;
+
+    const paperIds: string[] = [];
+    for (let group of info.value.groupIds)
+      paperIds.push.apply(paperIds, (await trpc.group.content.query({ id: group })).papers);
+    for (let paper of paperIds)
+      papers.value.push(await trpc.paper.content.query({ id: paper }));
+    paperLoading.value = false;
   } catch (err) {
     if (isTRPCClientError(err)) {
       ElMessage({ message: err.message, type: 'error', showClose: true });
